11c2f8ba5268c73c3e20a274df7ae65671234268
강동현 <dhkang@spidor.co.kr>
2024-06-22 00:24:30 +0900

    BaseCoroutine Exception 로거 추가

--- src/main/kotlin/supa/duap/BaseCoroutine.kt
+++ src/main/kotlin/supa/duap/BaseCoroutine.kt
@@ -1,11 +1,14 @@
 package supa.duap
 
+import io.ktor.util.logging.*
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
+import org.slf4j.LoggerFactory
 
 object BaseCoroutine {
+    private val logger = LoggerFactory.getLogger(BaseCoroutine::class.java)
     private val exceptionHandler : CoroutineExceptionHandler = CoroutineExceptionHandler { _, e ->
-
+        logger.error(e)
     }
 
     val ui = Dispatchers.Main + exceptionHandler
